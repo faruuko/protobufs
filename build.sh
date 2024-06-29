292438ab874286324eb845f25cbc9b042bce5411
Faruq Alli-Balogun <ab.faruq@sten.agency>
2024-06-29 17:31:00 +0100

    ..

--- build.sh
+++ build.sh
@@ -1,12 +1,11 @@
-# Script top create folder
-
-OUT_DIR=./auto-generated/proto-output
-
-mkdir -p $OUT_DIR
-
 #!/bin/bash
+
+OUTPUT_DIR=./generated/proto-ts
+
+mkdir -p $OUTPUT_DIR
+
 protoc --plugin=node_modules/ts-proto/protoc-gen-ts_proto \
- --ts_proto_out=$OUT_DIR \
+ --ts_proto_out=$OUTPUT_DIR \
  --ts_proto_opt=outputServices=grpc-js \
  --ts_proto_opt=esModuleInterop=true \
  -I=proto/ proto/*.proto proto/**/*.proto
